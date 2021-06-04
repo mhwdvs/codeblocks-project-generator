dcca25f0eae08db973e857372ffe3d02c1108c03
Matthew Davis <matt@mhwdvs.com>
2021-06-04 14:30:58 +0800

    Mostly-relative paths done, not a great solution

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import glob
 import pathlib
+import fileinput
+
+# get current path
+path = pathlib.Path().absolute().__str__() + "/"
+print("Current path: " + path)
 
 # check current path for any .cbp files
-print(pathlib.Path().absolute())
-
 targets = []
 for file in glob.glob("*.cbp"):
     targets.append(file)
     print("Found file: " + file)
 
+if len(targets) == 0:
+    print("No codeblocks projects found :|")
+    exit(1)
+
 # for each .cbp file
 for cbp in targets:
-    print("yo")
+    print("Making file :" + cbp + " relative")
 
-# get current path
+    # open file with write perms
+    with fileinput.FileInput(cbp, inplace=True, backup='.bak') as file:
+        # iterate through, removing current path wherever found (leaving relative path)
+        for line in file:
+            print(line.replace(path, ""), end='')
 
-# open file with write perms
+    print("Finished with file: " + cbp)
 
-# iterate through, removing current path wherever found (leaving relative path)
+
+print("All done!")
